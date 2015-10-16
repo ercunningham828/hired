606ca2dcf9140524b8873194cd2882abf8e67597
Emily Cunningham <ernoonan@gmail.com>
2015-10-16 14:21:20 -0500

    seeded job and applicant data

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,9 @@
 class Application < ActiveRecord::Base
   belongs_to :job
+  after_create :set_status
+
+  def set_status
+    self.status="New Application"
+    self.save
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,102 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+ 
+ # Create Ruler of Westeros Job
+  ruler=Job.create!(
+     title:  "Rule of Westeros",
+     description:   Faker::Lorem.paragraph
+   )
+  # Create Member of Nights Watch Job
+   member=Job.create!(
+     title:  "Member of the Night's Watch",
+     description:   Faker::Lorem.paragraph
+   )
+
+  #Create Warden of North Job
+    warden=Job.create!(
+     title:  "Warden of the North",
+     description:   Faker::Lorem.paragraph
+   )
+
+  #Create Lord Commander
+    commander=Job.create!(
+     title:  "Lord Commander of the Night's Watch",
+     description:   Faker::Lorem.paragraph
+   )
+
+
+ 
+ # Create Applications
+
+ daenerys=Application.create!(
+  name:"Daenerys Targaryen",
+  email: "motherofdragons@gmail.com",
+  phone: Faker::PhoneNumber.cell_phone,
+  job: ruler
+  )
+
+ joff=Application.create!(
+  name:"Joffrey Baratheon",
+  email: "kingjoffrey@kingslanding.com",
+  phone: Faker::PhoneNumber.cell_phone,
+  job: ruler
+  )
+
+  stannis=Application.create!(
+  name:"Stannis Baratheon",
+  email: "stantheman@hotmail.com",
+  phone: Faker::PhoneNumber.cell_phone,
+  job: ruler
+  )
+
+  ned=Application.create!(
+  name:"Eddard Stark",
+  email: "winteriscoming@yahoo.com",
+  phone: Faker::PhoneNumber.cell_phone,
+  job: member
+  )
+
+ samwell=Application.create!(
+  name:"Samwell Tarly",
+  email: "samtheslayer@gmail.com",
+  phone: Faker::PhoneNumber.cell_phone,
+  job: member
+  )
+
+roose=Application.create!(
+  name:"Roose Bolton",
+  email: "roose@flayedman.com",
+  phone: Faker::PhoneNumber.cell_phone,
+  job: warden
+  )
+
+theon=Application.create!(
+  name:"Theon Greyjoy",
+  email: "reek@sbcglobal.net",
+  phone: Faker::PhoneNumber.cell_phone,
+  job: warden
+  )
+
+jon=Application.create!(
+  name:"Jon Snow",
+  email: "youknownothing@nightswatch.com",
+  phone: Faker::PhoneNumber.cell_phone,
+  job: commander
+  )
+
+janos=Application.create!(
+  name:"Janos Slynt",
+  email: "janos@nightswatch.com",
+  phone: Faker::PhoneNumber.cell_phone,
+  job: commander
+  )
+
+bowen=Application.create!(
+  name:"Bowen Marsh",
+  email: "bowen@nightswatch.com",
+  phone: Faker::PhoneNumber.cell_phone,
+  job: commander
+  )
+
+ puts "Seed finished"
+ puts "#{Job.count} jobs created"
+ puts "#{Application.count} applications created"
